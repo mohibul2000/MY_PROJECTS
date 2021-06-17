d09af6d5233946c8fc5153ec8455f9b07789fa64
MOHIBUL AKTAR MOLLAH <66714292+mohibul2000@users.noreply.github.com>
2021-06-17 08:08:43 +0530

    Add files via upload

---
+++ JAVA_PROGRAM/WordCount.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class WordCount {
+    static int wordcount(String string) {
+        int count=0;
+
+        char ch[]= new char[string.length()];
+        for(int i=0;i<string.length();i++) {
+            ch[i]= string.charAt(i);
+            if( ((i>0)&&(ch[i]!=' ')&&(ch[i-1]==' ')) || ((ch[0]!=' ')&&(i==0)) )
+                count++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER A STRING : ");
+        String str = sc.nextLine();
+        System.out.println(wordcount(str) + " words.");
+    }
+}
